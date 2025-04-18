0850d819b98d375ae47a464cc37b9e269b0475e8
Freddy Torres <joanxtb@gmail.com>
2025-04-18 15:31:55 -0500

    fix bookings list avatar

--- src/components/properties/booking_list_item.tsx
+++ src/components/properties/booking_list_item.tsx
@@ -10,7 +10,7 @@ const BookingListItem: React.FC<BookingListItemProps> = ({ booking }) => {
     const { doctor } = booking;
     return (
         <div className="col-md-6 col-sm-12 col-lg-4 p-0" role="listitem">
-            <figure className="card p-2 m-1" style={{ background: `url(${doctor.photo}) no-repeat left center white`, overflow: 'hidden', border: '0px', height: 120 }}>
+            <figure className="card p-2 m-1" style={{ background: `url(${doctor.photo}) no-repeat left center white`, overflow: 'hidden', border: '0px', height: 120, backgroundSize: '120px' }}>
                 <section className="d-flex flex-column h-100" style={{ marginLeft: 130 }}>
                     <span className="black font-14 line1 anton">{`${doctor.name} ${doctor.lastname}`}</span>
                     <span className="gray font-12 line1">{`${doctor.specialty}`}</span>
