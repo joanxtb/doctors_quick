496331b53c99c4d8461dfc6d7ec8a2a24c1244cf
Freddy Torres <joanxtb@gmail.com>
2025-04-18 15:41:12 -0500

    fix an issue with avatar pictures after filtering

--- src/components/properties/booking_list_item.tsx
+++ src/components/properties/booking_list_item.tsx
@@ -8,9 +8,17 @@ interface BookingListItemProps {
 
 const BookingListItem: React.FC<BookingListItemProps> = ({ booking }) => {
     const { doctor } = booking;
+
+    const cardStyle: React.CSSProperties = {
+        background: `url(${doctor.photo}) left center / 120px no-repeat white`,
+        overflow: 'hidden',
+        border: '0px',
+        height: 120,
+    };
+
     return (
         <div className="col-md-6 col-sm-12 col-lg-4 p-0" role="listitem">
-            <figure className="card p-2 m-1" style={{ background: `url(${doctor.photo}) no-repeat left center white`, overflow: 'hidden', border: '0px', height: 120, backgroundSize: '120px' }}>
+            <figure className="card p-2 m-1" style={cardStyle}>
                 <section className="d-flex flex-column h-100" style={{ marginLeft: 130 }}>
                     <span className="black font-14 line1 anton">{`${doctor.name} ${doctor.lastname}`}</span>
                     <span className="gray font-12 line1">{`${doctor.specialty}`}</span>
--- src/components/properties/doctor_list_item.tsx
+++ src/components/properties/doctor_list_item.tsx
@@ -6,10 +6,19 @@ interface DoctorListItemProps {
     onBook: (Doctor) => void;
 }
 
+
 const DoctorListItem: React.FC<DoctorListItemProps> = ({ doctor, onBook }) => {
+
+    const cardStyle: React.CSSProperties = {        
+        background: `url(${doctor.photo}) left center / 120px no-repeat white`,
+        overflow: 'hidden',
+        border: '0px',
+        height: 120,
+    };
+
     return (
         <div className="col-md-6 col-sm-12 col-lg-4 p-0" role="listitem">
-            <figure aria-label={`book ${doctor.name} ${doctor.lastname}`} className="card p-2 m-1" style={{ background: `url(${doctor.photo}) no-repeat left center white`, overflow: 'hidden', border: '0px', height: 120, backgroundSize: '120px' }}>
+            <figure aria-label={`book ${doctor.name} ${doctor.lastname}`} className="card p-2 m-1" style={cardStyle}>
                 <section className="d-flex flex-column h-100" style={{ marginLeft: 130 }}>
                     <span className="black font-14 line1 anton">{`${doctor.name} ${doctor.lastname}`}</span>
                     <span className="gray font-12 line1">{`${doctor.specialty}`}</span>
